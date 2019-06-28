ca89d7f8fa06fbad935f591622bd9da22b9756c2
Dawid klatkiewicz <klatkiewiczdawid@gmail.com>
2019-06-28 19:14:37 -0400

    Made component to switch mode

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,25 @@
 <template>
   <div id="app">
-    <div id="switchpanel">
-      <button @click="goAdvanced()" v-if="simpleUi">Go Advanced</button>
-      <button @click="goSimple()" v-if="!simpleUi">Go Simple</button>
-    </div>
-    <router-view/>
+    <switchMode />
+    <router-view />
   </div>
 </template>
 
 <script>
+import switchMode from '@/components/switchMode.vue';
+
 export default {
   name: 'app',
   components: {
-    
+    switchMode,
   },
   data() {
     return {
-      simpleUi: false,
+      
     };
   },
   methods: {
-    goAdvanced() {
-      //console.log("hello world!");
-      this.simpleUi = false;
-      this.$router.push('/advanced');
-    },
-    goSimple() {
-      this.simpleUi = true;
-      this.$router.push('/');
-    },
+
   }
 }
 </script>
---
+++ src/components/switchMode.vue
@@ -0,0 +1,32 @@
+<template>
+   <div id="swtichMode">
+      <button @click="goAdvanced()" v-if="simpleUi">Go Advanced</button>
+      <button @click="goSimple()" v-if="!simpleUi">Go Simple</button>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "switchMode",
+   data() {
+      return {
+         simpleUi: false,
+      };
+   },
+   methods: {
+      goAdvanced() {
+         //console.log("hello world!");
+         this.simpleUi = false;
+         this.$router.push('/advanced');
+      },
+      goSimple() {
+         this.simpleUi = true;
+         this.$router.push('/');
+      },
+   }
+}
+</script>
+
+<style>
+
+</style>
