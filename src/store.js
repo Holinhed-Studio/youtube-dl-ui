041cc8b25e35fb6660a6f1739248361bb1b93d8f
Dovahkiin-Warrior <tony@firehostredux.net>
2019-06-28 22:08:05 -0400

    Major changes
    Added support for updating youtube-dl, prettified code

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 export default new Vuex.Store({
   state: {
@@ -11,7 +11,7 @@ export default new Vuex.Store({
     binary: '"./bin/youtube-dl"',
     params: new Array(),
     events: new Array(),
-	links: new Array(),
+    links: new Array()
   },
   mutations: {
     setParams(context, args) {
@@ -20,30 +20,53 @@ export default new Vuex.Store({
       //console.log(context.params);
       console.log("Params Set!  :" + context.params);
     },
-	getLink(context, args) {
-		const links = args.params.split(" ");
-		context.links = links;
-		console.log(context.links);		
-	}
+
+    getLink(context, args) {
+      const links = args.params.split(" ");
+      context.links = links;
+      console.log(context.links);
+    }
   },
   actions: {
-    testCall(context) {
+    updateBin(context) {
+      console.log("Updating youtube-dl binary..."),
+        exec(context.state.binary + " " + "-U", (err, stdout, stderr) => {
+          if (err) {
+            console.error(err.toString());
+            return;
+          }
 
+          // push to event log
+          context.state.events.push(stdout, stderr);
+
+          console.log(stdout);
+          console.log(stderr);
+        });
+    },
+    testCall(context) {
       //console.log(context);
 
-      console.log("Launching youtube-dl with parameters: " + context.state.params.join(" ") + context.state.links.join(" "));
-      exec(context.state.binary + " " + context.state.links.join(" "), + context.state.params.join(" "), (err, stdout, stderr) => {
+      console.log(
+        "Launching youtube-dl with parameters: " +
+          context.state.params.join(" ") +
+          context.state.links.join(" ")
+      );
+      exec(
+        context.state.binary + " " + context.state.links.join(" "),
+        +context.state.params.join(" "),
+        (err, stdout, stderr) => {
+          if (err) {
+            console.error(err.toString());
+            return;
+          }
 
-        if (err) { console.error(err.toString()); return; }
+          // push to event log
+          context.state.events.push(stdout, stderr);
 
-        // push to event log
-        context.state.events.push(stdout, stderr);
-
-        console.log(stdout);
-        console.log(stderr);
-
-      });
-
-    },
+          console.log(stdout);
+          console.log(stderr);
+        }
+      );
+    }
   }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,39 @@
 <template>
- <div class="home">
-    <input v-model="link" @input="getLink()" placeholder="Insert URL">
-    <button @click="testCall()" style="background-color:lightgreen"><b>Execute!</b></button>
+  <div class="home">
+    <input v-model="link" @input="getLink()" placeholder="Insert URL" />
+    <button @click="testCall()" style="background-color:lightgreen">
+      <b>Execute!</b>
+    </button>
+    <button @click="updateBin()" style="background-color:pink">
+      Update
+    </button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import store from '@/store';
+import store from "@/store";
 
 export default {
-  name: 'home',
-    components: {
-    
-    },
-    data() {
+  name: "home",
+  components: {},
+  data() {
     return {
-        link: "",
+      link: ""
     };
-     },
-    methods: {
-        testCall() {
-      
-        store.dispatch('testCall');
-
-        },
-        getLink() {
-        this.$store.commit('getLink', { params: this.link });
-        },
+  },
+  methods: {
+    testCall() {
+      store.dispatch("testCall");
     },
-}
+
+    updateBin() {
+        store.dispatch("updateBin");
+    },
+
+    getLink() {
+      this.$store.commit("getLink", { params: this.link });
+    }
+  }
+};
 </script>
